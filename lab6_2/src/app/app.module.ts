76b3c24be89f698cebffe6d37099ace3042b1e35
Eneren <enerencomplex@gmail.com>
2021-12-08 14:16:33 +0200

    ТОМАС ШЕЛБИ

---
+++ lab6_2/src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule }   from '@angular/forms'
+
+import { AppComponent } from './app.component'
+import { DisplayComponent } from './components/display/display.component'
+//import { SelectionComponent } from './components/selection/selection.component'
+
+import { HttpClientModule } from '@angular/common/http'
+import { WeatherDataService } from './services/weather-data.service'
+import { SelectionComponent } from './components/selection/selection.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DisplayComponent,
+    SelectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [WeatherDataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
